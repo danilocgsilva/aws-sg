2c1de2434445201de8bcce72b40ff3c90d4bcce5
Danilo Silva <danilocgsilva@gmail.com>
2020-05-16 18:30:35 -0300

    Already listing real data
    By the time now, you must have setted environments data for profile and
    region to works.

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,20 @@
-
+from src.Client_Config import Client_Config
+from src.Fetcher import Fetcher
+from src.SG_Parser import SG_Parser
 
 def main():
-    print("Hello World!")
+
+    client_config = Client_Config()
+    client = client_config.get_client()
+
+    fetcher = Fetcher()
+    fetcher.set_client(client)
+
+    sg_parser = SG_Parser()
+    sgs_data = fetcher.sgs_data()
+    sg_parser.set_data(sgs_data)
+    security_group_list = sg_parser.list()
+
+    for sg in security_group_list:
+        print(sg.get_name())
     
