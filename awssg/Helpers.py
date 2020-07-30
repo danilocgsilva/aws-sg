1c2c159502391e88244267b5c0bba348772cdf7f
Danilo Silva <danilocgsilva@gmail.com>
2020-07-30 09:51:29 -0300

    Coverage in gitignore, multiple vcs
    The .coverage has been added to the gitignore and also the application
    halts with exception in case of no or multiple virtual private clouds.
    This because if you have more than one virtual private cloud you may
    create the security group in the wrong place, and now this is not
    possible anymore. In the future, in case of multiple virtual private
    clouds, user may have an option to choose which vpc he desires to
    create the security group.

--- awssg/Helpers.py
+++ awssg/Helpers.py
@@ -81,7 +81,12 @@ def print_securities_groups_from_rds_instance(rds_name):
 def create_sg(group_name_description):
     ec2 = boto3.client('ec2')
     vpcs_response = ec2.describe_vpcs()
-    vpc_id = vpcs_response.get('Vpcs', [{}])[0].get('VpcId', '')
+    vpcs_containers = vpcs_response.get('Vpcs', [{}])
+
+    if len(vpcs_containers) != 1:
+        raise Exception("You have no or more than a single virtual private cloud in this account, and now this program does not support such situation...")
+
+    vpc_id = vpcs_containers[0].get('VpcId', '')
     ec2.create_security_group(
         GroupName=group_name_description, 
         Description=group_name_description, 
