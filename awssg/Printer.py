7453e34445e7d60227d745ad7b40852a2b7417a5
Danilo Silva <contact@danilocgsilva.me>
2020-08-04 10:11:07 -0300

    Invalid fields treatment
    Invalid fields arent accepted anymore and the right fields
    possibilities are informed to the user

--- awssg/Printer.py
+++ awssg/Printer.py
@@ -6,6 +6,8 @@ class Printer:
         self.sg = None
         self.name = False
         self.rules_count = False
+        self.valid_fields = ['name', 'rulescount']
+        self.fields_validated = None
 
     def set_sg(self, sg: SG):
         self.sg = sg
@@ -17,9 +19,9 @@ class Printer:
             return self
 
         each_fields = fields_string.split(",")
-        if 'name' in each_fields:
+        if self.valid_fields[0] in each_fields:
             self.name = True
-        if 'rulescount' in each_fields:
+        if self.valid_fields[1] in each_fields:
             self.rules_count = True
         return self
 
@@ -30,3 +32,25 @@ class Printer:
         if self.rules_count:
             return_string += "\n * Rules count: " + str(self.sg.get_rules_count())
         return return_string
+
+    def is_fields_valids(self) -> bool:
+        return self.fields_validated
+
+    def validate_fields(self, fields_string=None):
+        if fields_string is not None:
+            each_fields = fields_string.split(",")
+            for provided_filter in each_fields:
+                if not provided_filter in self.valid_fields:
+                    self.fields_validated = False
+                    break
+            if self.fields_validated is None:
+                self.fields_validated = True
+        else:
+            self.fields_validated = True
+
+    def get_invalid_fields_message(self) -> str:
+        exception_message = "There are invalid options in the fields options."
+        exception_message += "\nValid options are:"
+        for valid_option in self.valid_fields:
+            exception_message += "\n" + valid_option
+        return exception_message
