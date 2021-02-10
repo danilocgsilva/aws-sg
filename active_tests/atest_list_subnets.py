214aa92dc5b3bf4f0a51e0a41a880a55aa291c91
Danilo Silva <contact@danilocgsilva.me>
2021-02-10 08:35:31 -0300

    Better subnet object analisys

--- active_tests/atest_list_subnets.py
+++ active_tests/atest_list_subnets.py
@@ -20,15 +20,36 @@ if not args.vpc:
     print("You must specify a vpc name with --vpc or -v argument in commando line.")
     exit()
 
+def get_only_subnet_data(subnets_full_response):
+    return subnets_full_response["Subnets"]
+
+def get_subnets_ids(subnets_full_response):
+    list_subnets_ids = []
+    for subnet_data in subnets_full_response["Subnets"]:
+        list_subnets_ids.append(subnet_data["SubnetId"])
+    return list_subnets_ids
+
 os.environ['AWS_PROFILE'] = args.profile
 ec2_client = boto3.client('ec2', region_name=args.region)
 # os.environ['AWS_DEFAULT_REGION'] = args.region
 
-subnets = ec2_client.describe_subnets(
+subnets_full_response = ec2_client.describe_subnets(
     Filters=[{
         "Name": "vpc-id",
         "Values": [args.vpc]
     }]
 )
 
-print(subnets)
+
+print(
+    subnets_full_response
+)
+print("-----")
+print(
+    get_only_subnet_data(subnets_full_response)
+)
+print("-----")
+print(
+    get_subnets_ids(subnets_full_response)
+)
+print("-----")
